125eb846af4ff4f61681af609d6dd6d8a6781b70
Михалева Мария <mm@smarthead.ru>
2023-06-22 18:55:51 +0300

    add jwt strategy

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, HttpException, HttpStatus, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, HttpException, HttpStatus, UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('review')
 export class ReviewController {
@@ -17,10 +18,11 @@ export class ReviewController {
     async delete(@Param('id') id: string) {
         const deletedDoc = await this.reviewService.delete(id);
         if(!deletedDoc) {
-            throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND) // лучше сообщенияч выносить в отдельные константы, т.к. проще будет проверить
+            throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
         }
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('byProduct/:productId')
     async getbyProduct(@Param('productId') productId: string) {
         return this.reviewService.findByProductId(productId)
